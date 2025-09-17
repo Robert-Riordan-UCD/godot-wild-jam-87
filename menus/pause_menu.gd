46f85424eb571a71ae5b5b368fb6bb6d4973152a
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2025-09-17 12:38:35 +0200

    Added pause menu

---
+++ menus/pause_menu.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+class_name PauseMenu
+
+const MAIN_MENU = "res://menus/main_menu.tscn"
+
+@onready var paused: bool = false
+
+func _ready() -> void:
+	resume()
+
+func pause() -> void:
+	paused = true
+	visible = true
+	get_tree().paused = true
+
+func resume() -> void:
+	paused = false
+	visible = false
+	get_tree().paused = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		if paused: resume()
+		else: pause()
+
+func _on_resume_pressed() -> void:
+	resume()
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_main_menu_pressed() -> void:
+	visible = false
+	get_tree().paused = false
+	get_tree().change_scene_to_file(MAIN_MENU)
