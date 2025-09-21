0541d2a05d8829af9967e2acf99d24011274a695
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2025-09-21 21:22:18 +0200

    Other players removed in creative mode and board cleared

---
+++ modes/creative_setup.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name CreativeSetup
+
+@onready var players: Players = $"../GUI/Players"
+@onready var knot_tile_map: KnotTileMap = $"../Board/KnotTileMap"
+
+func _ready() -> void:
+	for i in range(1, players.get_child_count()):
+		players.get_child(i).queue_free()
+	
+	knot_tile_map.clear()
