a649672c6657f679f46366c01a9db27708e45162
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2025-09-21 16:19:26 +0200

    Added ease and transition type to scene transition

--- autoload/scene_transition.gd
+++ autoload/scene_transition.gd
@@ -8,14 +8,13 @@ func change_scene(new_scene: String, min_duration: float=1.5) -> void:
 	texture_rect.material.set_shader_parameter("right_bound", 0.0);
 	
 	var tween: Tween = create_tween()
-	tween.tween_property(texture_rect, "material:shader_parameter/right_bound", 1.0, min_duration/2.0)	
+	tween.tween_property(texture_rect, "material:shader_parameter/right_bound", 1.0, min_duration/2.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	
 	await tween.finished
 	get_tree().change_scene_to_file(new_scene)
-	get_tree().root.ready
 	
 	tween = create_tween()
-	tween.tween_property(texture_rect, "material:shader_parameter/left_bound", 1.0, min_duration/2.0	)	
+	tween.tween_property(texture_rect, "material:shader_parameter/left_bound", 1.0, min_duration/2.0).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
 	await tween.finished
 	
 	visible = false
