c36065672f7dc9577cb5ed1ce96a8abaf561043b
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2025-11-24 17:13:46 +0100

    Passing doesn't end the game in creative mode

--- modes/creative_setup.gd
+++ modes/creative_setup.gd
@@ -7,6 +7,7 @@ class_name CreativeSetup
 
 func _ready() -> void:
 	tutorial.queue_free()
+	players.game_over.disconnect(get_parent()._on_game_over)
 	
 	for i in range(1, players.get_child_count()):
 		players.get_child(i).queue_free()
